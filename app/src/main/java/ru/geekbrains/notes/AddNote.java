3c03090b39ada7f459b5d5302d72fdccc2b93116
Aleksey <alexcorpetf@gmail.com>
2021-03-24 22:25:05 +1000

    Новая заметка
    Создание новой заметки доделано. Работает

--- app/src/main/java/ru/geekbrains/notes/AddNote.java
+++ app/src/main/java/ru/geekbrains/notes/AddNote.java
@@ -1,15 +1,22 @@
 package ru.geekbrains.notes;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
 
 public class AddNote extends Fragment {
 
@@ -48,13 +55,22 @@ public class AddNote extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_add_note, container, false);
         FloatingActionButton fab_save_note = view.findViewById(R.id.fab_save_note);
+        EditText noteTitle = view.findViewById(R.id.note_add_title);
+        EditText noteAbout = view.findViewById(R.id.note_add_about);
+        EditText noteContent = view.findViewById(R.id.note_add_content);
         fab_save_note.setOnClickListener(v -> {
             FragmentManager fragmentManager = getParentFragmentManager();
             fragmentManager.popBackStack();
-            fab_save_note.setVisibility(View.INVISIBLE);
+            LinkedList<String> note = new LinkedList<>();
+            Date dateNow = new Date();
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+            note.add(formatForDateNow.format(dateNow));
+            note.add(noteTitle.getText().toString());
+            note.add(noteAbout.getText().toString());
+            note.add(noteContent.getText().toString());
+            Notes.allNotes.add(note);
         });
         return view;
     }
